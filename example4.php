550438c7819c0f0bbd56ab73a30a26c2c63b7539
Drew Powers <drew@dangodesign.net>
2013-12-19 08:22:12 -0500

    Fix syntax error from 11 mos ago

--- example4.php
+++ example4.php
@@ -10,7 +10,7 @@ if($connect) {
 	$sysid = '156456';
 	$n = 1;
 	$dir = 'photos/'.$sysid;
-	if(!is_dir($dir)) mkdir($dir) // Remember: this can only make one directory at a time
+	if(!is_dir($dir)) mkdir($dir); // Remember: this can only make one directory at a time
 	$photos = $rets->GetObject('Property', 'Photo', $sysid);
 	foreach($photos as $photo) {
 		file_put_contents($dir.'/'.$n.'.jpg', $photo['Data']);
